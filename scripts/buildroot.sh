aa75c97a240041d689281fc669a7f99730c3c403
Florido Paganelli <florido.paganelli@hep.lu.se>
2023-07-12 11:21:15 +0200

    Added some checks to the buildscript for root

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -4,6 +4,17 @@
 # The paths are initialized there.
 # TODO: Can be generalized by passing the ROOT version for the source path.
 
+for var_name in BUILDDIR APPS SOURCEDIR; do
+  if [ -z "${!var_name}" ]; then   
+     echo "$var_name is not set. Please run"
+     echo "   export $var_name=/path/to/dir"
+     echo "before running this script. Exiting..."
+     exit 1
+  fi
+done
+
+exit
+
 mkdir -p ${BUILDDIR}/root ${APPS}/root
 cd ${BUILDDIR}/root
 cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX=${APPS}/root ${SOURCEDIR}/root-6.28.04/
